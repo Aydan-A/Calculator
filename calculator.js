d0150a48b9685029a8c35531a0c3e6361da631bc
Aydan Abbaslı <aydan26.abbasova@gmail.com>
2024-10-14 11:46:04 -0400

    Calculator is created

---
+++ calculator.js
@@ -0,0 +1,80 @@
+let Resultdisplay = document.querySelector(".maindisplay");
+let sequenceOfOperations = document.querySelector(".sequenceOfOperations");
+let AllbuttonsContainer = document.querySelector(".buttons-grid");
+
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+let equalsPressed = false;
+
+function calculate(prev, curr, operator) {
+  prev = parseFloat(prev);
+  curr = parseFloat(curr);
+
+  switch (operator) {
+    case "+":
+      return prev + curr;
+    case "-":
+      return prev - curr;
+    case "*":
+      return prev * curr;
+    case "/":
+      return prev / curr;
+    case "%":
+      return prev * (curr / 100);
+    case "√x ":
+      return Math.sqrt(curr);
+    default:
+      return curr;
+  }
+}
+
+AllbuttonsContainer.addEventListener("click", function (e) {
+  let el = e.target;
+  let elText = el.innerHTML;
+
+  if (isFinite(elText) || elText === ".") {
+    currentValue += elText;
+    Resultdisplay.value = currentValue;
+  } else if (elText !== "=" && elText !== "del" && elText !== "C") {
+    if (currentValue !== "") {
+      operator = elText;
+      previousValue = currentValue;
+      sequenceOfOperations.value = previousValue + " " + operator;
+      currentValue = "";
+      equalsPressed = false;
+    }
+  } else if (elText === "=") {
+    if (previousValue !== "" && currentValue !== "" && operator !== "") {
+      let result = calculate(previousValue, currentValue, operator);
+      sequenceOfOperations.value =
+        previousValue + " " + operator + " " + currentValue;
+      currentValue = result;
+      Resultdisplay.value = result;
+      equalsPressed = true;
+    }
+  } else if (elText === "del") {
+    if (currentValue.length > 0) {
+      currentValue = currentValue.slice(0, -1); // Remove the last character
+      Resultdisplay.value = currentValue;
+    }
+  } else if (elText === "C") {
+    clearAll();
+  }
+});
+
+function sequence(e) {
+  let el = e.target;
+  let elText = el.innerHTML;
+  if (elText !== "=" && elText !== "del") {
+    sequenceOfOperations.value += elText;
+  }
+}
+
+function clearAll() {
+  Resultdisplay.value = "";
+  sequenceOfOperations.value = "";
+  currentValue = "";
+  previousValue = "";
+  operator = "";
+}
